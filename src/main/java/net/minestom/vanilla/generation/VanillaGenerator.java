19149683a85b143ed0dc5e4480dae0512fc51d3b
KrystilizeNevaDies <57762380+KrystilizeNevaDies@users.noreply.github.com>
2020-07-04 11:04:31 +1000

    Revert "Cleanup"
    This reverts commit 9368d8668f26edb8035bc7a754200cf3de7eb41b.

--- src/main/java/net/minestom/vanilla/generation/VanillaGenerator.java
+++ src/main/java/net/minestom/vanilla/generation/VanillaGenerator.java
@@ -15,7 +15,7 @@ import java.util.Random;
 
 
 public class VanillaGenerator extends ChunkGenerator  {
-
+	
 	int seed = (int) (47758);
 	private Random random = new Random(seed);
 	double seedProduct = random.nextDouble();
@@ -25,27 +25,27 @@ public class VanillaGenerator extends ChunkGenerator  {
     	for (byte x = 0; x < Chunk.CHUNK_SIZE_X; x++) {
             for (byte z = 0; z < Chunk.CHUNK_SIZE_Z; z++) {
 
-
+            	
                 int posX = (chunkX*16+x);
                 int posZ = (chunkZ*16+z);
-
-
+                
+            	
                 int height = getHeight(posX, posZ);
-
+                
                 int crust = (int) (height - 4);
-
-
-
+                
+                
+                
                 for (int level = 0; level < crust; level++) {
                     batch.setBlock(posX, level, posZ, Block.STONE);
                 }
-
+                
                 for (int level = crust; level <= height; level++) {
                     batch.setBlock(posX, level, posZ, Block.DIRT);
                 }
-
+                
                 batch.setBlock(posX, height, posZ, Block.GRASS_BLOCK);
-
+                
                 if (bushCheck(batch, posX/2, posZ/2) == true) {
                 	batch.setBlock(posX, height + 1, posZ, Block.GRASS);
                 }
@@ -65,18 +65,18 @@ public class VanillaGenerator extends ChunkGenerator  {
     		return false;
     	}
     }
-
+    
     private int getHeight(int posX, int posZ) {
         double heightDelta = getNoiseValue(posX, posZ, 16);
         double heightAlpha = getNoiseValue(posX, posZ, 256);
         int height = (int) (2 * Math.abs(heightDelta) + 16 * Math.abs(heightAlpha) + 80);
         return height;
     }
-
+    
     private double getNoiseValue(int posX, int posZ, double NoiseID) {
     	return noise.getNoise((seed + posX) / NoiseID, (seed + posZ) / NoiseID);
     }
-
+    
     @Override
     public List<ChunkPopulator> getPopulators() {
         return null;
