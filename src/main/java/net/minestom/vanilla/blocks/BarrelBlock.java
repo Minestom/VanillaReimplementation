2d70bbd6d847a4fb006779e110795c63fa7718fe
KrystilizeNevaDies <57762380+KrystilizeNevaDies@users.noreply.github.com>
2020-07-14 03:01:05 +1000

    debugging

--- src/main/java/net/minestom/vanilla/blocks/BarrelBlock.java
+++ src/main/java/net/minestom/vanilla/blocks/BarrelBlock.java
@@ -7,6 +7,7 @@ import net.minestom.server.instance.block.Block;
 import net.minestom.server.inventory.Inventory;
 import net.minestom.server.utils.BlockPosition;
 import net.minestom.server.utils.Direction;
+import net.minestom.server.utils.MathUtils;
 import net.minestom.vanilla.blockentity.ChestBlockEntity;
 import net.minestom.vanilla.inventory.DoubleChestInventory;
 import net.minestom.vanilla.system.EnderChestSystem;
@@ -20,7 +21,40 @@ public class BarrelBlock extends ChestLikeBlock {
     public boolean dropContentsOnDestroy() {
         return true;
     }
+    
+    @Override
+    protected BlockPropertyList createPropertyValues() {
+        return new BlockPropertyList().facingProperty("facing").booleanProperty("open");
+    }
+    
+    @Override
+    public short getVisualBlockForPlacement(Player player, Player.Hand hand, BlockPosition position) {
+    	float yaw = player.getPosition().getYaw();
+    	float pitch = player.getPosition().getPitch();
+    	boolean open = false; // TODO: Open Barrel
+    	Direction direction = MathUtils.getHorizontalDirection(yaw).opposite();    	
 
+    	
+    	if (pitch > 45) {
+    		direction = Direction.DOWN;
+    	} else if (pitch < -45) {
+    		direction = Direction.UP;
+    	}
+    	
+    	System.out.println(String.valueOf(false));
+    	
+        return getBaseBlockState()
+        		.with("facing", direction.name().toLowerCase())
+        		.with("open", String.valueOf(false))
+        		.getBlockId();
+    }
+    
+    @Override
+    public boolean onInteract(Player player, Player.Hand hand, BlockPosition blockPosition, Data data) {
+        player.openInventory(getInventory(player, blockPosition, data));
+        return true;
+    }
+    
     @Override
     public Data createData(Instance instance, BlockPosition blockPosition, Data data) {
         return new ChestBlockEntity(blockPosition);
