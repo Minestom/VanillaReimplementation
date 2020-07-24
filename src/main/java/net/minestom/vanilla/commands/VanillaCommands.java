523fee11dcc756b6a0157fccae2837c506b47cb7
KrystilizeNevaDies <57762380+KrystilizeNevaDies@users.noreply.github.com>
2020-07-24 11:29:12 +1000

    Revert "commands and testing"
    This reverts commit e280d53a0b69e636c4a3063744eab018d540a114.

--- src/main/java/net/minestom/vanilla/commands/VanillaCommands.java
+++ src/main/java/net/minestom/vanilla/commands/VanillaCommands.java
@@ -3,8 +3,6 @@ package net.minestom.vanilla.commands;
 import net.minestom.server.command.CommandManager;
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.Command;
-import net.minestom.server.instance.block.Block;
-import net.minestom.vanilla.blocks.ShulkerBoxBlock;
 
 import java.util.function.Supplier;
 
@@ -19,9 +17,6 @@ public enum VanillaCommands {
     STOP(StopCommand::new),
     HELP(HelpCommand::new),
     SAVE_ALL(SaveAllCommand::new),
-    WHISPER(() -> new WhisperCommand("whisper")),
-    MSG(() -> new WhisperCommand("msg")),
-    TELL(() -> new WhisperCommand("tell")),
     ;
 
     private final Supplier<Command<? extends CommandSender>> commandCreator;
@@ -34,8 +29,7 @@ public enum VanillaCommands {
      * Register all vanilla commands into the given manager
      * @param manager
      */
-    @SuppressWarnings("unchecked")
-	public static void registerAll(CommandManager manager) {
+    public static void registerAll(CommandManager manager) {
         for(VanillaCommands vanillaCommand : values()) {
             Command<? extends CommandSender> command = vanillaCommand.commandCreator.get();
             manager.register((Command<CommandSender>) command);
