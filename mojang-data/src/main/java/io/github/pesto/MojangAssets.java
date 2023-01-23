0c2209450d14ac222a7aed7fd3008a389de27af9
_Pesto <pestonotpasta@gmail.com>
2023-01-23 20:33:30 +0300

    Add MojangAssets extraction tool

---
+++ mojang-data/src/main/java/io/github/pesto/MojangAssets.java
@@ -0,0 +1,182 @@
+package io.github.pesto;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static java.nio.file.StandardOpenOption.*;
+
+
+public final class MojangAssets {
+    private static final String VERSION_MANIFEST_URL = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
+
+    public static void downloadResources(String version, @NotNull File root) {
+        try {
+            // Get version info
+            String versionInfoUrl = findVersionInfoUrl(version);
+            JsonObject versionInfo = downloadJson(versionInfoUrl);
+
+            // Check if source files already exist
+            if (!validateSources(root, versionInfo)) {
+
+                // Download jar
+                System.out.println("Downloading vanilla jar...");
+                File jar = downloadJar(versionInfo, root);
+
+                // Extract files
+                System.out.println("Extracting assets from jar...");
+                extractJarAssets(jar, root);
+
+                // Clean up
+                jar.delete();
+            }
+        } catch (IOException e) {
+            exitError(e.getMessage());
+        }
+    }
+
+    private static boolean validateSources(File root, JsonObject versionInfo) {
+        String version = versionInfo.get("id").getAsString();
+        File file = new File(root, version + File.separator + ".mcassetsroot");
+        return file.exists();
+    }
+
+    /**
+     * Gets the version info from the version manifest
+     *
+     * @param version The release version or latest version if requested
+     * @return The url to the version info
+     * @throws IOException If the version info url could not be found
+     */
+    private static String findVersionInfoUrl(@NotNull String version) throws IOException {
+        // Get manifest
+        JsonObject manifest = downloadJson(VERSION_MANIFEST_URL);
+
+        // Get the latest version if requested
+        if (version.equals("latest")) {
+            JsonObject latest = manifest.getAsJsonObject("latest");
+            version = latest.get("release").getAsString();
+        }
+
+        // Find the url for the version's info
+        for (JsonElement elem : manifest.getAsJsonArray("versions")) {
+            JsonObject info = elem.getAsJsonObject();
+            String id = info.get("id").getAsString();
+
+            if (!id.equals(version))
+                continue;
+
+            return info.get("url").getAsString();
+        }
+
+        throw new IOException("Failed to find version info url for version " + version);
+    }
+
+    /**
+     * Downloads the vanilla jar to be used for extracting
+     *
+     * @param versionInfo The version info
+     */
+    private static File downloadJar(JsonObject versionInfo, @NotNull File root) throws IOException {
+        JsonObject downloads = versionInfo.getAsJsonObject("downloads");
+        JsonObject client = downloads.getAsJsonObject("client");
+        String url = client.get("url").getAsString();
+
+        String version = versionInfo.get("id").getAsString();
+        File destination = new File(root, version + File.separator + "resources.jar");
+        if (checkOrCreate(destination))
+            return destination;
+
+        try (
+                ReadableByteChannel in = Channels.newChannel(new URL(url).openStream());
+                FileChannel channel = FileChannel.open(destination.toPath(), CREATE, WRITE, TRUNCATE_EXISTING)
+        ) {
+            channel.transferFrom(in, 0, Long.MAX_VALUE);
+
+            boolean success = destination.exists() && destination.length() > 0;
+            if (!success)
+                throw new IOException("Failed to download client JAR");
+        }
+        return destination;
+    }
+
+    private static boolean checkOrCreate(@NotNull File file) throws IOException {
+        if (file.exists())
+            return true;
+
+        file.getParentFile().mkdirs();
+        if (!file.exists())
+            return file.createNewFile();
+
+        return true;
+    }
+
+    private static File checkAndCreateFile(File destination, ZipEntry entry) throws IOException {
+        String path = entry.getName().replace("data/", "").replace("minecraft/", "");
+        File destFile = new File(destination, path);
+
+        String dirPath = destination.getCanonicalPath();
+        String filePath = destFile.getCanonicalPath();
+
+        if (!filePath.startsWith(dirPath + File.separator)) {
+            throw new IOException("Entry outside target: " + entry.getName());
+        }
+
+        return destFile;
+    }
+
+    private static void extractJarAssets(@NotNull File jarFile, File root) {
+        File output = new File(root, jarFile.getParentFile().getName());
+
+        try (ZipInputStream zip = new ZipInputStream(new FileInputStream(jarFile))) {
+            ZipEntry entry;
+            while ((entry = zip.getNextEntry()) != null) {
+
+                // Skip unrelated entries
+                if (!entry.getName().startsWith("data"))
+                    continue;
+
+                // Validate that the file exists
+                File file = checkAndCreateFile(output, entry);
+                if (file.exists()) continue;
+
+                if (entry.isDirectory()) {
+                    if (!file.isDirectory() && !file.mkdirs())
+                        throw new IOException("Failed to create directory " + file);
+                } else {
+                    File parent = file.getParentFile();
+                    if (!parent.isDirectory() && !parent.mkdirs())
+                        throw new IOException("Failed to create directory " + parent);
+
+                    Files.copy(zip, file.toPath());
+                }
+            }
+            zip.closeEntry();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static JsonObject downloadJson(String url) throws IOException {
+        return JsonParser.parseReader(new InputStreamReader(getDownloadStream(url))).getAsJsonObject();
+    }
+
+    private static InputStream getDownloadStream(String url) throws IOException {
+        return new URL(url).openStream();
+    }
+
+    private static void exitError(String message) {
+        System.err.println(message);
+        System.exit(1);
+    }
+}
--- mojang-data/src/main/java/io/github/pesto/MojangDataFeature.java
+++ mojang-data/src/main/java/io/github/pesto/MojangDataFeature.java
@@ -5,7 +5,15 @@ import net.minestom.vanilla.VanillaRegistry;
 import net.minestom.vanilla.VanillaReimplementation;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MojangDataFeature implements VanillaReimplementation.Feature {
+    public static final File root = new File(".", "cache");
+
+    public static void main(String[] args) throws IOException {
+        MojangAssets.downloadResources("latest", root);
+    }
 
     @Override
     public void hook(@NotNull VanillaReimplementation vri, @NotNull VanillaRegistry registry) {
