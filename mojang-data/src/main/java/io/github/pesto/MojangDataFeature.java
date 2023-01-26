d1ace21c456a41c4546f7d158f641ffb20e16f78
Pesto <56796924+devPesto@users.noreply.github.com>
2023-01-26 06:21:36 +0300

    Add mojang-data module (#19)
    Add options to download various versions of mojand data, including an api to safely access it.
    In the future we may also want to cache the downloaded data on the filesystem.

---
+++ mojang-data/src/main/java/io/github/pesto/MojangDataFeature.java
@@ -0,0 +1,34 @@
+package io.github.pesto;
+
+import io.github.pesto.files.ByteArray;
+import io.github.pesto.files.FileSystem;
+import net.minestom.server.utils.NamespaceID;
+import net.minestom.vanilla.VanillaRegistry;
+import net.minestom.vanilla.VanillaReimplementation;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.CompletableFuture;
+
+public class MojangDataFeature implements VanillaReimplementation.Feature {
+
+    private static final String LATEST = "1.19.2";
+    private final MojangAssets assets = new MojangAssets();
+
+    @Override
+    public void hook(@NotNull VanillaReimplementation vri, @NotNull VanillaRegistry registry) {
+        assets.getAssets(LATEST).join();
+    }
+
+    @Override
+    public @NotNull NamespaceID namespaceID() {
+        return NamespaceID.from("io.github.pesto:mojang_data");
+    }
+
+    public CompletableFuture<FileSystem<ByteArray>> getAssets(@NotNull String version) {
+        return assets.getAssets(version);
+    }
+
+    public CompletableFuture<FileSystem<ByteArray>> getLatest() {
+        return assets.getAssets(LATEST);
+    }
+}
