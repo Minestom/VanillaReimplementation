977a328a365ad9312e4f0493b6c8bb7072ac93ca
KrystilizeNevaDies <tyreece@rozycki.family>
2023-12-27 23:58:14 +1000

    simplify dependency sorting

--- core/src/main/java/net/minestom/vanilla/utils/DependencySorting.java
+++ core/src/main/java/net/minestom/vanilla/utils/DependencySorting.java
@@ -11,70 +11,29 @@ public class DependencySorting {
         Set<T> dependencies();
     }
 
-    private record SortableSortable<T>(NamespaceDependent<T> dependant, Set<NamespaceDependent<T>> dependencies, Set<NamespaceDependent<T>> dependants) {
-        public SortableSortable(NamespaceDependent<T> dependant, Set<NamespaceDependent<T>> dependants) {
-            this(dependant, new HashSet<>(), new HashSet<>());
-            for (T dependency : dependant.dependencies()) {
-                NamespaceDependent<T> dependencyNamespaceDependent = getWhere(dependants, feat -> feat.dependencies().contains(dependency));
-                if (dependencyNamespaceDependent == null) throw new IllegalStateException("Sortable " + dependant.identity() + " depends on " + dependency + " which does not exist");
-                dependencies.add(dependencyNamespaceDependent);
-            }
-            for (NamespaceDependent<T> other : dependants) {
-                if (other.dependencies().contains(dependant.identity())) {
-                    this.dependants.add(other);
-                }
-            }
-        }
-
-        private static <T> T getWhere(Set<T> set, Predicate<T> predicate) {
-            for (T t : set) {
-                if (predicate.test(t)) return t;
-            }
-            return null;
-        }
-
-        public boolean hasDependencies() {
-            return !dependencies.isEmpty();
-        }
-    }
-
-    private record Sortable<I, ND extends NamespaceDependent<I>>(ND value, I identity, Set<I> dependencies, Set<I> dependants) {
-        public Sortable(ND value, Set<ND> all) {
-            this(value, value.identity(), new HashSet<>(value.dependencies()), new HashSet<>());
-            for (NamespaceDependent<I> other : all) {
-                if (other.dependencies().contains(identity)) {
-                    dependants.add(other.identity());
-                }
-            }
-        }
-    }
-
     public static <T, ND extends NamespaceDependent<T>> List<ND> sort(Set<ND> dependants) {
-        List<ND> out = new ArrayList<>();
-        Queue<Sortable<T, ND>> remaining = new PriorityQueue<>(Comparator.comparingInt(o -> o.dependencies().size()));
+        List<ND> sorted = new ArrayList<>();
 
         for (ND dependant : dependants) {
-            if (dependant.dependencies().isEmpty()) out.add(dependant);
-            else remaining.add(new Sortable<>(dependant, dependants));
-        }
+            // find the index of the first element that depends on this element
+            int index = -1;
+            for (int i = 0; i < sorted.size(); i++) {
+                if (sorted.get(i).dependencies().contains(dependant.identity())) {
+                    index = i;
+                    break;
+                }
+            }
 
-        while (!remaining.isEmpty()) {
-            Sortable<T, ND> dependant = remaining.poll();
-            if (dependant.dependencies().isEmpty()) {
-                out.add(dependant.value);
+            // if no element depends on this element, add it to the very start
+            if (index == -1) {
+                sorted.add(dependant);
                 continue;
             }
-            for (ND completed : out) {
-                dependant.dependencies().remove(completed.identity());
-            }
-            if (dependant.dependencies().isEmpty()) {
-                out.add(dependant.value);
-            } else {
-                remaining.add(dependant);
-            }
+
+            // else add it before the first element that depends on it
+            sorted.add(index, dependant);
         }
 
-        if (out.size() != dependants.size()) throw new IllegalStateException("Circular dependency detected");
-        return out;
+        return List.copyOf(sorted);
     }
 }
