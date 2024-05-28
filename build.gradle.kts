df710429479ef89d8d5e956241e0c7d831eb36f1
KrystilizeNevaDies <tyreece@rozycki.family>
2024-05-28 23:17:37 +1000

    update to java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@ subprojects {
     version = "indev"
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
 
 //        withJavadocJar()
         withSourcesJar()
